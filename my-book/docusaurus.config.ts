86bef74490b1a364d6016a04b073d85c947a0413
M-Kashif-ALI <158289645+M-Kashif-ALI@users.noreply.github.com>
2025-12-25 19:45:14 +0500

    Update docusaurus.config.ts

--- my-book/docusaurus.config.ts
+++ my-book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://hakathon1-tawny.vercel.app/', // GitHub Pages URL
+  url: 'https://hakathon1-tawny.vercel.app', // GitHub Pages URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
